9ed8b9593ffe3a847c9527a0ab2227e22b843e75
xbakhromjon <xbakhromjon@gmail.com>
2024-02-14 16:44:28 +0500

    corrected

--- main.go
+++ main.go
@@ -8,7 +8,5 @@ import (
 func main() {
 	log.Fatal(http.ListenAndServe(":5005", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("pong"))
-
-		sas
 	})))
 }
