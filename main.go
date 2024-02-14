f134eb6937b0944644da26e04961294422d76ab7
xbakhromjon <xbakhromjon@gmail.com>
2024-02-14 17:35:50 +0500

    .

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"log"
-	"net/http"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":5005", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("pong"))
-	})))
+	log.Println("CD worked")
 }
