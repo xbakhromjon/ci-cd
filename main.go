e69e55b16643e8006df8b9d5097e532bec9e9035
xbakhromjon <xbakhromjon@gmail.com>
2024-02-16 15:11:41 +0500

    pong

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	log.Fatal(http.ListenAndServe(":5005", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("ping"))
+		writer.Write([]byte("pong"))
 	})))
 }
